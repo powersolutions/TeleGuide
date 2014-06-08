71ed882b11f54566f5cc6f0870454233b6dac60d
ShakthiM91 <coolgang.shakthi@gmail.com>
2014-06-08 23:38:44 +0530

    spalsh screen added

--- src/com/snapdragons/teleguide/MainActivity.java
+++ src/com/snapdragons/teleguide/MainActivity.java
@@ -3,21 +3,30 @@ package com.snapdragons.teleguide;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	ImageView img;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		img=(ImageView)findViewById(R.id.imageView1);
+		img.setOnClickListener(this);
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
 		
 	}
 
---
+++ src/com/snapdragons/teleguide/Splash.java
@@ -0,0 +1,70 @@
+package com.snapdragons.teleguide;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class Splash extends Activity implements OnClickListener {
+
+	VideoView videoView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+
+		init();
+
+		new CountDownTimer(17000, 1000) { // adjust the milli seconds here
+
+			@Override
+			public void onTick(long millisUntilFinished) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void onFinish() {
+				startActivity(new Intent(Splash.this, MainActivity.class));
+			}
+
+		}.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+
+	private void init() {
+		String uriPath = "android.resource://" + getPackageName() + "/"
+				+ R.raw.mm;
+
+		videoView = (VideoView) findViewById(R.id.videoView1);
+		// MediaController mediaController = new MediaController(this);
+		// mediaController.setAnchorView(videoView);
+		// URI either from net
+		Uri video = Uri.parse(uriPath);
+		// videoView.setMediaController(mediaController);
+		videoView.setVideoURI(video);
+		videoView.start();
+
+		videoView.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		startActivity(new Intent(Splash.this, MainActivity.class));
+	}
+
+}
